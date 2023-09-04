0ef9e50ba698a0c458ac15ace4cf4037153b38d6
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-04 17:59:23 +0200

    Task 2 done

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    print('"Programming is like building a multilingual puzzle\n')
+    print('"Programming is like building a multilingual puzzle')
