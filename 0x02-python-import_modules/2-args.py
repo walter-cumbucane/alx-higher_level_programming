f676a7b042ed06bbb7a00f65dce499d00675dfcb
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-07 20:45:11 +0200

    Task 2 done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
     number_arguments = len(argv) - 1
     if number_arguments == 0:
         print(f"{number_arguments:d} arguments.")
+    elif number_arguments == 1:
+        print(f"{number_arguments:d} argument:")
+        print(f"1: {argv[1]}")
     else:
         print(f"{number_arguments:d} arguments:")
         i = 1
