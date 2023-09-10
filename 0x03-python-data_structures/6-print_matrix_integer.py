1124cabf44a1e81b941970f175d6b70b65b990d9
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-10 23:49:33 +0200

    Task 6 done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
         print()
     else:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                print("{:d}".format(j), end=" ")
+        for lst in matrix:
+            for num in lst:
+                print("{:d}".format(num), end=" ")
             print("")
