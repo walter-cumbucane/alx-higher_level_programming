c0901ae5b1b9fb6e87d05f310f8c564bcdf2c3b5
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-10 23:06:23 +0200

    Task 4 done

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    copied_list = my_list.copy()
+    copied_list[idx] = element
+    return copied_list
