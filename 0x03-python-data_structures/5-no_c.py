07e04baf0ea4396af93d19ba532f6463296b4bc1
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-10 23:33:05 +0200

    Task 5 done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,8 @@ def no_c(my_string):
     no_cs = []
     delimeter = ""
     for char in my_string:
-        if char == "c":
+        if char == "c" or char == "C":
             continue
         no_cs.append(char)
     new_string = delimeter.join(no_cs)
+    return new_string
