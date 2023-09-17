16fc662406129ff5ffb099b3676e3daa9338515a
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-18 01:26:27 +0200

    Task 10 done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,11 @@
 
 def best_score(a_dictionary):
     largest = None
+    key_name = None
     if not a_dictionary:
-        return largest
+        return key_name
     for key in a_dictionary:
         if largest is None or a_dictionary[key] > largest:
             largest = a_dictionary[key]
-    return largest
+            key_name = key
+    return key_name
