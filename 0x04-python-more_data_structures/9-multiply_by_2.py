ecb9498bbb6e89fe98368f9693c4f8064764d1b9
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-18 01:20:25 +0200

    Task 9 done

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for key in a_dictionary:
-        new_dict[key] = a_dictionay[key] * 2
+        new_dict[key] = a_dictionary[key] * 2
     return new_dict
