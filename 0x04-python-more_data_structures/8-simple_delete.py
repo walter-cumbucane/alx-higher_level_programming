dea2f4c46f8d449747ca68651941060f49fcf7db
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-18 01:15:13 +0200

    Task 8 done

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,7 @@
 def simple_delete(a_dictionary, key=""):
     if len(key) == 0:
         return a_dictionary
+    if not key in a_dictionary:
+        return a_dictionary
     del a_dictionary[key]
     return a_dictionary
