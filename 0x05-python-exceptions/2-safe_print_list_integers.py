3f095ed4a2155f6b1840da04f39b3c69943e16f4
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-25 19:35:44 +0200

    Task 2 done

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,17 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         i = 0
+        printed_element = 0
         while i != x:
-            print(f"{my_list[i]}", end="")
-            i = i + 1
-    except (ValueError, TypeError):
+            if isinstance(my_list[i], int):
+                print(f"{my_list[i]}", end="")
+                printed_element = printed_element + 1
+                i = i + 1
+            else:
+                i = i + 1
+                continue
+    except (ValueError):
         pass
     finally:
         print("")
-        return i
+        return printed_element
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def list_division(my_list1, my_list2, list_length):
+    
