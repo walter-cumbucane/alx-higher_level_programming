c1a3b1b8f426476340dccef2854b0533a2e97920
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-25 19:40:53 +0200

    Task 2 done

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        i = 0
-        printed_element = 0
-        while i != x:
-            if isinstance(my_list[i], int):
-                print(f"{my_list[i]}", end="")
-                printed_element = printed_element + 1
-                i = i + 1
-            else:
-                i = i + 1
-                continue
-    except (ValueError):
-        pass
+    i = 0
+    while i != x:
+        try:
+            print(f"{my_list[i]}", end="")
+            i = i + 1
+        except (ValueError, TypeError):
+            continue
     finally:
         print("")
-        return printed_element
+        return i
