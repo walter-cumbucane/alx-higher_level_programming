b3579490236c8f2321480e87a4812d3436a4ca86
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-25 21:54:55 +0200

    Task 9  done

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,12 +2,14 @@
 
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 2):
-        if i > a:
-            raise Exception("Too far")
-        else:
-            intermedium = a ** b
-        result += intermedium / i
-    finally:
-        result = a + b
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                intermedium = a ** b
+                result += intermedium / i
+        except Exception:
+            result = a + b
+            break
         return result
