9abbb363b58294e39704fa780e09a01e74df0f56
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-26 23:50:38 +0200

    Task 1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This code defines a Square class with an attribute inside"""
+
+
+class Square:
+    """This is the constructor of the class"""
+    def __init__(self, size):
+        self.__size = size
