3ca1d47d1bdbc804cab5d01cfd093fbf6bbdf394
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-27 00:11:15 +0200

    Task 3 done

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This code defines a Square class"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    Methods:
+        area: returns the square area
+    """
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the square's area
+        Args:
+            None
+        Returns:
+            The square's area
+        """
+        area = self.__square * self.__square
+        return area
