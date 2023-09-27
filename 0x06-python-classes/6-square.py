e5ca3818be2274ad3ca0fa4a8590d46163bc7e7b
walter-cumbucane <cumbucane.walter@gmail.com>
2023-09-27 22:04:42 +0200

    Task 6 done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,7 +73,7 @@ class Square:
             print("")
         else:
             for y in range(self.__position[1]):
-                print("", end ="")
+                print()
             for x in range(self.__size):
                 print(' ' * self.__position[0], end='')
                 print('#' * self.__size)
