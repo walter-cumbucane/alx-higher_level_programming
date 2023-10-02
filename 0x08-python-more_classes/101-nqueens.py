512acbf3209c436149452c81bbb3b055bdfafc82
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-03 00:28:39 +0200

    Task 10 in the making...

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,3 +4,11 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
+try:
+    num = int(sys.argv[1])
+except Exception as e:
+    print("N must be a number")
+    exit(1)
+if num < 4:
+    print("N must be at least 4")
+    exit(1)
