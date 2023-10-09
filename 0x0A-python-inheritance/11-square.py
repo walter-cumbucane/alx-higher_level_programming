cd6372fcdeb23f10a54d2ae15bf97c2a65d3d1de
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-09 23:44:51 +0200

    Task 11 done

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,9 +1,47 @@
 #!/usr/bin/python3
 """This module implements a square method"""
-base_class = __import__("9-rectangle").Rectangle
 
 
-class Square(base_class):
+class BaseGeometry(object):
+    """empty class"""
+
+    def __init__(self):
+        "Constructor"
+        super().__init__()
+
+    def area(self):
+        """Calculates the area of the geometric figure"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A public instance method"""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Implements a rectangle class"""
+    def __init__(self, width, height):
+        """class's constructor"""
+        super().__init__()
+        super().integer_validator("height", height)
+        super().integer_validator("width", width)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """implements the area's method"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """To make the instance printable"""
+        printable = "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return printable
+
+
+class Square(Rectangle):
     """Implements a square class"""
     def __init__(self, size):
         """class's constructor"""
