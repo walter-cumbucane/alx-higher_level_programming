b182995dcaffde9600f3ea18f027899453a436ef
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-09 23:34:45 +0200

    Task 11 done

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module implements a square method"""
+base_class = __import__("9-rectangle").Rectangle
+
+
+class Square(base_class):
+    """Implements a square class"""
+    def __init__(self, size):
+        """class's constructor"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Makes the instance printable"""
+        printable = "[square] {}/{}".format(self.__size, self.__size)
+        return printable
