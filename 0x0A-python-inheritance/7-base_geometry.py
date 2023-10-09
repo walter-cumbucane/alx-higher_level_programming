d28ab91b5b18eac592efcca2c350c36245550721
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-09 22:55:16 +0200

    Task 7 done

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains a BaseGeometry class"""
+
+
+class BaseGeometry(object):
+    """empty class"""
+
+    def __init__(self):
+        "Constructor"
+        super().__init__()
+
+    def area(self):
+        """Calculates the area of the geometric figure"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A public instance method"""
+        if not isinstance(value, int):
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
