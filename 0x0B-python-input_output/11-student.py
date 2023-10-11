6cadbd826d43376278b286ea1329af6c9d43302e
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-11 04:00:59 +0200

    Task 9 done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module contains a class"""
+
+
+class Student(object):
+    """Defines a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """class's constructor"""
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of the object"""
+        if type(attrs) is list:
+            to_return = dict()
+            attributes = self.__dict__
+            for key in attributes:
+                if key in attrs:
+                    to_return[key] = attributes[key]
+            return to_return
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the object"""
+        for key in json:
+            self.__init__[key] = json[key]
