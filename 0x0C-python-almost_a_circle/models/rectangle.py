9c0b96d8ce91849e60f09971737172c03193fb90
walter-cumbucane <cumbucane.walter@gmail.com>
2023-10-16 03:55:08 +0200

    Cooking...

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,10 +25,10 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if y <= 0:
             raise ValueError("y must be > 0")
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     """Width getter"""
     @property
