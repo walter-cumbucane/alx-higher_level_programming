9ff248e38ed260dc93d3da993eb6e4f8f9c1ea9e
walter-cumbucane <cumbucane.walter@gmail.com>
2023-11-17 01:42:43 +0200

    Task 8 done

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- uses the count MySQL function to count the number of rows that
+-- returns from a SQL Query
+-- Syntax: SELECT COUNT(*) FROM table_name WHERE condition;
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
