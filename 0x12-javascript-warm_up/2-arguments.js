9952f02749e4d233ebd799dc80e7abccbbd679d2
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-11 22:29:14 +0200

    Task 2 done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const process = require('process');
+const cmdArguments = process.argv
+if (cmdArguments.length === 2) {
+  console.log('No argument');
+} else if (cmdArguments.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
