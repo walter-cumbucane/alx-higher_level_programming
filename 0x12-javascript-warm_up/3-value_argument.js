c38232b2146dd5b3c0d6458016f42719222a4473
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-11 22:53:59 +0200

    Task 3 done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const process = require('process')
-const cmdArguments = process.argv
+const process = require('process');
+const cmdArguments = process.argv;
 
-if (cmdArguments[2] === undefined){
-  console.log("No argument");
+if (cmdArguments[2] === undefined) {
+  console.log('No argument');
 } else {
   console.log(cmdArguments[2]);
 }
