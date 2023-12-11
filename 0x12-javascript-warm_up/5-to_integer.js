6687210b129c78b7dda5c4ddfc64ba4abbd1acd7
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-12 00:09:26 +0200

    Task 5 done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,9 +4,9 @@ const process = require('process');
 const cmdArgs = process.argv;
 const arg = cmdArgs[2];
 
-if (isNaN(arg)){
+if (isNaN(arg)) {
   console.log('Not a number');
-} else  {
+} else {
   const finalNumber = parseInt(arg);
   console.log('My number: ', finalNumber);
 }
