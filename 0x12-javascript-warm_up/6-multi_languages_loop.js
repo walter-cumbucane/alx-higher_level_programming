ef3e86a4eb07e2a88461c9c2c7a7053a370b77bd
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-12 00:19:22 +0200

    Task 6 done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < languages.length; i++)
-{
+for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);
 }
