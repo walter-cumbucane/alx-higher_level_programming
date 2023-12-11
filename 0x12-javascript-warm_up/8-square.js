bfaf358ed12519afca23828a19b739cc6aa19752
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-12 00:32:58 +0200

    Task 8 done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+const size = args[2];
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
