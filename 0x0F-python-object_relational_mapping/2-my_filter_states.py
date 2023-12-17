7e7585c035352efc2e50adc321601ee1a2de490f
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-17 21:48:05 +0200

    Task 2 done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Contains a script that interacts with a database
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name={%s} ORDER BY id ASC"
+    cur.execute(sql, (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
