90ae2386293141341449429ab7feb065ff0a2178
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-17 21:33:44 +0200

    Task 1 done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ def main():
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    sql = "SELECT * FROM states ORDER BY id ASC WHERE id=4 AND id=5"
+    sql = "SELECT * FROM states WHERE id=4 OR id=5 ORDER BY id ASC"
     cur.execute(sql)
     rows = cur.fetchall()
     for row in rows:
