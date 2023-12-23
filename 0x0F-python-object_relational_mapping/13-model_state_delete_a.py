bbcd4357057939a30ac078698f6199e2ea47edc1
walter-cumbucane <cumbucane.walter@gmail.com>
2023-12-23 19:34:12 +0200

    Task 13 done

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Delets records from a database using SQLAlchemy
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+def main():
+    """
+        Main Function
+    """
+    conn = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(conn)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
+        session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
