ebf7716081cf225f5ecf228fc6ef5632e11a6de3
walter-cumbucane <cumbucane.walter@gmail.com>
2024-02-13 21:02:27 +0200

    Task 2 done

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
