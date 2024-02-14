ba23c0c7713d52a00af9d7de23e4d0ea000b2f3a
walter-cumbucane <cumbucane.walter@gmail.com>
2024-02-15 00:39:57 +0200

    Task 8 done

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,7 +4,7 @@ module.exports.esrever = function (list) {
   const newList = [];
   const len = list.length;
   for (let i = len - 1; i > 0; i--) {
-    newList[len - 1 - i] = list[i];
+    newList.push(list[i]);
   }
   return newList;
 };
