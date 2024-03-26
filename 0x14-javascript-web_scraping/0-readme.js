368e8e07d7027cd1be25c63b219c83e166920d31
walter-cumbucane <cumbucane.walter@gmail.com>
2024-03-26 23:58:25 +0200

    Task 0 done

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
