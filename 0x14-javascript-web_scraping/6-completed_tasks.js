95b00f994b42f5b55d906b69b3b42c27532e60d0
walter-cumbucane <cumbucane.walter@gmail.com>
2024-03-27 00:56:03 +0200

    Task 6 done

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,22 +3,21 @@ const request = require('request');
 
 const url = process.argv[2];
 request(url, (err, res, body) => {
-  if (err)  {
+  if (err) {
     console.log(err);
   }
   const completedTasksByUsers = {};
   body = JSON.parse(body);
   for (let i = 0; i < body.length; ++i) {
-      const userId = body[i].userId;
-      const completed = body[i].completed;
+    const userId = body[i].userId;
+    const completed = body[i].completed;
 
-      if (completed && !completedTasksByUsers[userId]) {
-        completedTasksByUsers[userId] = 0;
-      }
-
-      if (completed) ++completedTasksByUsers[userId];
+    if (completed && !completedTasksByUsers[userId]) {
+      completedTasksByUsers[userId] = 0;
     }
 
-    console.log(completedTasksByUsers);
+    if (completed) ++completedTasksByUsers[userId];
   }
+
+  console.log(completedTasksByUsers);
 });
