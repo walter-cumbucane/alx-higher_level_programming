735be7bc43bf1b13b50657e47fe9ba9ceb03d602
walter-cumbucane <cumbucane.walter@gmail.com>
2024-03-27 00:53:34 +0200

    Task 6 done

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err)  {
+    console.log(err);
+  }
+  const completedTasksByUsers = {};
+  body = JSON.parse(body);
+  for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
+    }
+
+    console.log(completedTasksByUsers);
+  }
+});
