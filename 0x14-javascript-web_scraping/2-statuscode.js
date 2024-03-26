134d6711f0decd9312d058e9525eac700e9fa751
walter-cumbucane <cumbucane.walter@gmail.com>
2024-03-27 00:14:28 +0200

    Task 2 done

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,8 @@ const request = require('request');
 
 const url = process.argv[2];
 request(url, (err, res) => {
+  if (err) {
+    console.log(err);
+  }
   console.log(`code: ${res.statusCode}`);
 });
