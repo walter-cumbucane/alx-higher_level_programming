94ad1230086b0090c25f343c1989b73048454ecc
walter-cumbucane <cumbucane.walter@gmail.com>
2024-03-27 00:46:03 +0200

    Task 5 done

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  body = JSON.parse(body);
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
